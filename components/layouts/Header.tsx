bc249515f1908eb6107855cbe392493d5480a744
nikhilsharma-03 <sharmanikhil3406@gmail.com>
2026-06-18 17:59:42 +0530

    header UI updated

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,9 +1,25 @@
 "use client"
 import React from 'react'
 import "./Header.css"
+import { BiHomeAlt } from "react-icons/bi";
+import { FaRegUser } from "react-icons/fa6";
+import { FiShoppingCart } from "react-icons/fi";
+
+
 const Header: React.FC = () => {
   return (
-    <div>Header</div>
+    <>
+    <div className="main">
+      <div className="box1">
+        <div className="cart_icon"><BiHomeAlt /></div>
+        <div className="title">ShopEase</div>
+      </div>
+      <div className="box2">
+        <div className="user_icon"><FaRegUser /></div>
+        <div className="cart_icon"><FiShoppingCart /></div>
+      </div>
+    </div>
+    </>
   )
 }
 
