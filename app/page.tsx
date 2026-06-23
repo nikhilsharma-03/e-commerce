75d311c6ecfb1e30499559de768b07e9ea6a454c
nikhilsharma-03 <sharmanikhil3406@gmail.com>
2026-06-23 18:32:56 +0530

    banner created

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import HomeBanner from "@/components/homeComponents/HomeBanner";
 import Header from "@/components/layouts/Header";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <>
     <Header />
+    <HomeBanner />
     </>
   );
 }
