d7bc82e44864e4f008a4269b0e686ca586ff6222
nikhilsharma-03 <sharmanikhil3406@gmail.com>
2026-06-26 18:33:45 +0530

    footer created

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import HomeBanner from "@/components/homeComponents/HomeBanner";
 import HomeCategory from "@/components/homeComponents/HomeCategory";
+import Footer from "@/components/layouts/Footer";
 import Header from "@/components/layouts/Header";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -9,6 +9,7 @@ export default function Home() {
     <Header />
     <HomeBanner />
     <HomeCategory />
+    <Footer />
     </>
   );
 }
