382b09e21ffc59989541a11005ac1d78e9d57b7d
nikhilsharma-03 <sharmanikhil3406@gmail.com>
2026-06-26 16:46:16 +0530

    category images created

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import HomeBanner from "@/components/homeComponents/HomeBanner";
+import HomeCategory from "@/components/homeComponents/HomeCategory";
 import Header from "@/components/layouts/Header";
 import Image from "next/image";
 
@@ -7,6 +8,7 @@ export default function Home() {
     <>
     <Header />
     <HomeBanner />
+    <HomeCategory />
     </>
   );
 }
